c80a52e7ffa56bd3e4cc163faf033753d5a79c99
khatrisomay <somaykhatri6555@gmail.com>
2026-07-26 21:19:39 +0530

    tf: Add security groups for EKS & ALB

---
+++ deployments/terraform/security_groups.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "alb" {
+  name        = "${var.project_name}-alb-sg-${var.environment}"
+  description = "Security group for external Application Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg-${var.environment}"
+  }
+}
+
+resource "aws_security_group" "eks_nodes" {
+  name        = "${var.project_name}-eks-nodes-sg-${var.environment}"
+  description = "Security group for EKS worker nodes"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow traffic from ALB"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-eks-nodes-sg-${var.environment}"
+  }
+}
