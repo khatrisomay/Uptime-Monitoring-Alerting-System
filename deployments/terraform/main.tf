35fe39b23b036db1499a39688df566d942357c2f
khatrisomay <somaykhatri6555@gmail.com>
2026-07-26 21:18:19 +0530

    tf: Configure AWS provider & tags

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
