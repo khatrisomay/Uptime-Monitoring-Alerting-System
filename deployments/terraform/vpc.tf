abdd72101e1da8db37c23654a927008d75ceb447
khatrisomay <somaykhatri6555@gmail.com>
2026-07-26 21:18:52 +0530

    tf: Add VPC & subnets module

---
+++ deployments/terraform/vpc.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc-${var.environment}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                     = "${var.project_name}-public-subnet-${count.index + 1}"
+    "kubernetes.io/role/elb"                 = "1"
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                     = "${var.project_name}-private-subnet-${count.index + 1}"
+    "kubernetes.io/role/internal-elb"        = "1"
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared"
+  }
+}
